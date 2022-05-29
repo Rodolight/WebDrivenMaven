fe35c66dc9db1a978edf4aa9214172450d286c5e
Rodolfo De Peña <arieshome@Rodolfos-MBP.attlocal.net>
2022-05-29 08:53:37 -0400

    Initial commit

---
+++ src/test/java/loginTest.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class loginTest {
+	
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp() {
+		//FOR BEST FUCNTION THE DRIVER MUST BE LOCATED ON /usr/local/bin folder 
+		driver = new FirefoxDriver();
+	}
+	
+	@Test
+	public void doLogin() {
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("identifierId")).sendKeys("rodoldo.depena@gmail.com");
+		driver.findElement(By.className("zQJV3")).click();
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+		driver.quit();
+	}
+	
+
+}
